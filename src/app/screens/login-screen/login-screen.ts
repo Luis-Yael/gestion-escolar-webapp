b5a9dce65e95f3179f1d7cffdb7448f9d34cc066
Luis Yael Mendez Sanchez <luisyaelms@gmail.com>
2026-03-11 14:59:06 -0600

    Front del login
    Se agrega todo el diseño de la vista login

---
+++ src/app/screens/login-screen/login-screen.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-screen',
+  imports: [
+    ...SHARED_IMPORTS
+  ],
+  templateUrl: './login-screen.html',
+  styleUrl: './login-screen.scss',
+})
+export class LoginScreen implements OnInit {
+
+  // Aquí van las variables globales
+  public username: string = '';
+  public password: string = '';
+  public load: boolean = false;
+  public errors: any = {};
+  public type: string = "password";
+
+  constructor(
+    public router: Router
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+  public login(){
+
+  }
+
+  public registrar() {
+
+  }
+
+  public showPassword() {
+
+  }
+
+}
